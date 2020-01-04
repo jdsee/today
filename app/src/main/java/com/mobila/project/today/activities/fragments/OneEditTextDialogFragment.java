90b95b2614ac011d566d744124ab0d2e2663fac5
JoschaS <s0566845@htw-berlin.de>
2020-01-04 16:14:16 +0100

    modiefied OneEditTextDialogFragment

--- app/src/main/java/com/mobila/project/today/activities/fragments/OneEditTextDialogFragment.java
+++ app/src/main/java/com/mobila/project/today/activities/fragments/OneEditTextDialogFragment.java
@@ -1,4 +1,3 @@
-
 package com.mobila.project.today.activities.fragments;
 import android.app.AlertDialog;
 import android.os.Bundle;
@@ -11,6 +10,8 @@ import com.mobila.project.today.R;
 import java.util.Objects;
 
 public class OneEditTextDialogFragment extends GeneralConfirmationDialogFragment {
+    public static final String EDIT_TEXT_HINT = "HINT_STRING";
+
     public static final String CONFIRMED_STRING = "CONFIRMATION_STRING";
 
     private OneEditTextDialogListener callback;
@@ -19,9 +20,10 @@ public class OneEditTextDialogFragment extends GeneralConfirmationDialogFragment
     @Override
     void getOptionalDialogAddendum(Bundle bundle, AlertDialog.Builder builder) {
         LayoutInflater inflater = Objects.requireNonNull(getActivity()).getLayoutInflater();
-        View view = inflater.inflate(R.layout.dialog_add_course, null);
+        View view = inflater.inflate(R.layout.dialog_one_edit_text, null);
         builder.setView(view);
-        this.editText = view.findViewById(R.id.course_name_edit_text);
+        this.editText = view.findViewById(R.id.edit_text);
+        this.editText.setHint(bundle.getString(EDIT_TEXT_HINT));
     }
 
     @Override
